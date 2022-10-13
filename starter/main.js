18eb68209dc243044a715a3222fc9c9156e05b1c
ozovehepaul <114989613+ozovehepaul@users.noreply.github.com>
2022-10-13 13:44:13 +0100

    completed hamburger section.

---
+++ starter/main.js
@@ -0,0 +1,19 @@
+
+// ES5 function
+
+
+// console.log("send me your account number")
+
+
+// var writeup= "I am hungry"
+
+
+var hamburgerdiv = document.querySelector(".hamburger")
+
+console.log(hamburgerdiv)
+
+function dosomething()  {
+    hamburgerdiv.classList.toggle("changeburger")
+
+    // alert(writeup)
+}
