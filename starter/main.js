7e9b4b732f0b97d88108d27dcff1248b11a68014
ozovehepaul <114989613+ozovehepaul@users.noreply.github.com>
2022-10-14 12:44:38 +0100

    completed header section for large and smaller screens

--- starter/main.js
+++ starter/main.js
@@ -8,12 +8,15 @@
 // var writeup= "I am hungry"
 
 
-var hamburgerdiv = document.querySelector(".hamburger")
-
-console.log(hamburgerdiv)
+var hamburgerdiv = document.querySelector(".hamburger");
+var mobileLinks = document.querySelector(".mobile-links-holder");
+var mybackdrop = document.querySelector(".backdrop");
+// console.log(hamburgerdiv)
 
 function dosomething()  {
     hamburgerdiv.classList.toggle("changeburger")
+    mobileLinks.classList.toggle("show-mobile-links-holder")
+    mybackdrop.classList.toggle("show-backdrop")
 
     // alert(writeup)
 }
